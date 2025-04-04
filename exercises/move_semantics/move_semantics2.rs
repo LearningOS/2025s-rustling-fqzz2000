039f4c5fdf04658d28ba087b7721ca126e109643
Quanzhi <fqzz2000@outlook.com>
2025-04-03 23:20:54 -0400

    finished structs

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -12,6 +12,7 @@ fn main() {
     let vec0 = Vec::new();
 
     let mut vec1 = fill_vec(vec0.clone());
+    let vec0 = vec1.clone();
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -20,8 +21,8 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+
 
     vec.push(22);
     vec.push(44);
